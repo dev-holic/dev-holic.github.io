fb5f3ea979e0d81c3892feb7494094ba133b8f28
dev-holic <yong1000abc@gmail.com>
2024-05-25 19:12:36 +0900

    HashRouter로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { Main } from "./main/Main";
 
 export function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/test" element={'test'} />
           <Route path="/test/:id" element={'test detail'} />
         </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
