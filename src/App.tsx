58cf8631e58ef9b1ee1173d0054d3f8540c658ef
yckim <yckim@miridih.com>
2024-08-25 15:03:21 +0900

    chore: scss 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import { Main } from './pages/main/Main';
+import { Plarium } from './pages/plarium/Plarium';
 
 export function App() {
   return (
     <HashRouter>
       <Routes>
         <Route path='/' element={<Main />} />
+        <Route path='/plarium' element={<Plarium />} />
         <Route path='/test' element={'test'} />
         <Route path='/test/:id' element={'test detail'} />
       </Routes>
