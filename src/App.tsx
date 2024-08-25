4bdf4181d77112f69a3acbbbbd22d186ad07d493
yckim <yckim@miridih.com>
2024-08-25 13:19:41 +0900

    chore: install prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
-import { Main } from "./main/Main";
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import { Main } from './main/Main';
 
 export function App() {
   return (
     <HashRouter>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/test" element={'test'} />
-          <Route path="/test/:id" element={'test detail'} />
-        </Routes>
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/test' element={'test'} />
+        <Route path='/test/:id' element={'test detail'} />
+      </Routes>
     </HashRouter>
-  )
+  );
 }
