5af2a24adc038d79b0ad584075b5b2776217f796
dev-holic <yong1000abc@gmail.com>
2024-08-28 22:43:57 +0900

    feat: 캔버스 클릭하면 이미지 이동하는 기능 추가

--- src/pages/canvas/Canvas.tsx
+++ src/pages/canvas/Canvas.tsx
@@ -1,23 +1,139 @@
-import { useEffect, useRef } from 'react';
+import { MouseEvent, useCallback, useEffect, useRef, useState } from 'react';
 
 export const Canvas = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const canvasSize = useCanvasSize();
+  const [imgInfo, setImgInfo] = useState<{
+    img: HTMLImageElement;
+    size: { width: number; height: number };
+  }>();
+  const imgPositionRef = useRef<{ x: number; y: number }>();
+  const clickIdRef = useRef<number>();
+  const rotationRef = useRef<number>(0);
+
+  const renderCanvas = useCallback(() => {
+    const ctx = canvasRef.current?.getContext('2d');
+    const imgPosition = imgPositionRef.current;
+    if (!ctx || !imgInfo || !imgPosition) {
+      return;
+    }
+    ctx.clearRect(0, 0, canvasSize.width, canvasSize.height);
+    ctx.save();
+    ctx.translate(imgPosition.x, imgPosition.y);
+    ctx.rotate(rotationRef.current);
+    ctx.drawImage(
+      imgInfo.img,
+      -imgInfo.size.width / 2,
+      -imgInfo.size.height / 2,
+      imgInfo.size.width,
+      imgInfo.size.height
+    );
+    ctx.restore();
+  }, [canvasSize, imgInfo]);
 
   useEffect(() => {
-    const ctx = canvasRef.current?.getContext('2d');
-    if (!ctx) {
+    loadImage('/choonsik.png').then((img) => {
+      const ratio = img.width / img.height;
+      const width = 100;
+      const height = width / ratio;
+      setImgInfo({ img, size: { width, height } });
+      if (!imgPositionRef.current) {
+        imgPositionRef.current = { x: canvasSize.width / 2, y: canvasSize.height / 2 };
+      }
+      renderCanvas();
+    });
+  }, [canvasSize.height, canvasSize.width, renderCanvas]);
+
+  const onClick = (event: MouseEvent) => {
+    const canvas = canvasRef.current;
+    const imgPosition = imgPositionRef.current;
+    if (!canvas || !imgPosition) {
       return;
     }
 
-    drawImage(ctx);
-  }, []);
+    const clickId = Date.now();
+    clickIdRef.current = clickId;
 
-  return <canvas ref={canvasRef} width={800} height={600} />;
+    const rect = canvas.getBoundingClientRect();
+    const startPosition = { ...imgPosition };
+    const endPosition = {
+      x: event.clientX - rect.left,
+      y: event.clientY - rect.top,
+    };
+    const speed = 5;
+    const rotationSpeed = 0.1;
+    const xDirection = endPosition.x > startPosition.x ? 1 : -1;
+    const yDirection = endPosition.y > startPosition.y ? 1 : -1;
+    const ratio = Math.abs((endPosition.y - startPosition.y) / (endPosition.x - startPosition.x));
+    const xMove = ratio > 1 ? speed / ratio : speed;
+    const yMove = ratio > 1 ? speed : speed * ratio;
+
+    const render = () => {
+      requestAnimationFrame(() => {
+        if (clickIdRef.current !== clickId) {
+          return;
+        }
+        let needRender = false;
+
+        if (xDirection > 0) {
+          if (imgPosition.x < endPosition.x) {
+            imgPosition.x = Math.min(imgPosition.x + xMove, endPosition.x);
+            needRender = true;
+          }
+        } else {
+          if (imgPosition.x > endPosition.x) {
+            imgPosition.x = Math.max(imgPosition.x - xMove, endPosition.x);
+            needRender = true;
+          }
+        }
+
+        if (yDirection > 0) {
+          if (imgPosition.y < endPosition.y) {
+            imgPosition.y = Math.min(imgPosition.y + yMove, endPosition.y);
+            needRender = true;
+          }
+        } else {
+          if (imgPosition.y > endPosition.y) {
+            imgPosition.y = Math.max(imgPosition.y - yMove, endPosition.y);
+            needRender = true;
+          }
+        }
+
+        if (needRender) {
+          rotationRef.current += rotationSpeed;
+          renderCanvas();
+          render();
+        }
+      });
+    };
+
+    render();
+  };
+
+  return (
+    <canvas ref={canvasRef} width={canvasSize.width} height={canvasSize.height} onClick={onClick} />
+  );
 };
 
-const drawImage = async (ctx: CanvasRenderingContext2D) => {
-  const img = await loadImage('/choonsik.png');
-  ctx.drawImage(img, 0, 0);
+const useCanvasSize = () => {
+  const [canvasSize, setCanvasSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setCanvasSize({ width: window.innerWidth, height: window.innerHeight });
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return canvasSize;
 };
 
 const loadImage = (src: string) => {
