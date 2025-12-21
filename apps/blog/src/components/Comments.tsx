8bb271cdf681e7fd4b68cd8fa9085d2baa4a7354
dev-holic <yong1000abc@gmail.com>
2025-12-21 21:49:20 +0900

    댓글 ui 변경

--- apps/blog/src/components/Comments.tsx
+++ apps/blog/src/components/Comments.tsx
@@ -21,13 +21,12 @@ export default function Comments() {
     script.setAttribute('data-strict', '0');
     script.setAttribute('data-reactions-enabled', '1');
     script.setAttribute('data-emit-metadata', '0');
-    script.setAttribute('data-input-position', 'top');
+    script.setAttribute('data-input-position', 'bottom');
     script.setAttribute('data-theme', 'preferred_color_scheme');
     script.setAttribute('data-lang', 'ko');
-    script.setAttribute('data-loading', 'lazy');
 
     ref.current.appendChild(script);
   }, []);
 
-  return <section ref={ref} className="mt-16 w-full" />;
+  return <section ref={ref} className='mt-16 w-full' />;
 }
